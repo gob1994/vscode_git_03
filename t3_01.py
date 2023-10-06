0591b58e5519ffb88a67dcd93451b1bd407da5ba
abc <abc@abc.com>
2023-10-06 23:54:34 +0800

    three_commit

--- t3_01.py
+++ t3_01.py
@@ -1,2 +1,6 @@
 import numpy as np
 print(np.random.randint(1,100,size=(3,4)))
+#引入pandas,并创建一个5行4列的dataframe
+import pandas as pd
+df = pd.DataFrame(np.random.randint(1,100,size=(5,4)),columns=list('ABCD'))
+print(df)
---
+++ t3_02.py
@@ -0,0 +1,5 @@
+import numpy as np
+# 创建一个5行4列的零矩阵
+a = np.zeros((5,4),dtype=int)
+# 打印矩阵
+print(a)
