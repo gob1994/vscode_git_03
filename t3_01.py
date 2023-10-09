ae54266aff04b2d4415bcda2ffa7dded17f5fc9a
gob1994 <31816778@qq.com>
2023-10-09 18:23:24 +0800

    增加笔记本文件测试08

--- t3_01.py
+++ t3_01.py
@@ -3,5 +3,3 @@ print(np.random.randint(1,100,size=(3,4)))
 import pandas as pd
 df = pd.DataFrame(np.random.randint(1,100,size=(5,4)),columns=list('ABCD'))
 print(df)
-import numpy as np
-np.random.randint
