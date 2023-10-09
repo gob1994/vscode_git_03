18594a1f7eaf632b04e240733518502e09dda015
gob1994 <31816778@qq.com>
2023-10-09 10:19:06 +0800

    提交添加第二个文件99

--- t3_02.py
+++ t3_02.py
@@ -1,5 +1,6 @@
 import numpy as np
-# 创建一个5行4列的零矩阵
-a = np.zeros((5,4),dtype=int)
-# 打印矩阵
+# 使用pandas的dataframe创建一个6行5列的整数随机数矩阵
+from pandas import DataFrame
+#给矩阵添加行索引和列索引
+a = DataFrame(np.random.randint(0,10,size=(6,5)),index=['a','b','c','d','e','f'],columns=['A','B','C','D','E'])
 print(a)
